d719bfa6d7d7e9a9db5bf471bc450ee54bec2b16
Oxters <oxters168@gmail.com>
2020-10-24 01:04:57 +0300

    Created visual studio project. Added a label and text box to the form. Found a script for getting mouse data and emulating mouse actions. Found a script for getting keyboard input. Created an async loop that starts when the form is created.

---
+++ BlenderGISMacro/MacroForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BlenderGISMacro
+{
+    partial class MacroForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MacroForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Blender GIS Macro";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // MacroForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 347);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MacroForm";
+            this.Text = "Blender GIS Macro";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MacroForm_FormClosing);
+            this.Resize += new System.EventHandler(this.MacroForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BlenderGISMacro/MacroForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlenderGISMacro
+{
+    public partial class MacroForm : Form
+    {
+        private List<KeyboardHook.VK> keysDown = new List<KeyboardHook.VK>();
+        private CancellationTokenSource backgroundProcess;
+
+        public MacroForm()
+        {
+            InitializeComponent();
+            KeyboardHook.CreateHook();
+            KeyboardHook.onKeyDown += KeyboardHook_onKeyDown;
+            KeyboardHook.onKeyUp += KeyboardHook_onKeyUp;
+
+            backgroundProcess = new CancellationTokenSource();
+            FrameRunner(backgroundProcess.Token);
+        }
+
+        private void MacroForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            KeyboardHook.DestroyHook();
+            backgroundProcess.Cancel();
+        }
+
+        private void OnUpdate()
+        {
+            //label1.Text = MouseOperations.GetCursorPosition().ToString();
+            //label1.Text = ToComboString(keysDown);
+            //MoveMouseWithButtons();
+        }
+
+        private void MoveMouseWithButtons()
+        {
+            var currentMousePosition = MouseOperations.GetCursorPosition();
+            int xOffset = 0, yOffset = 0;
+            if (keysDown.Contains(KeyboardHook.VK.VK_DOWN))
+                yOffset = 10;
+            if (keysDown.Contains(KeyboardHook.VK.VK_UP))
+                yOffset = -10;
+            if (keysDown.Contains(KeyboardHook.VK.VK_LEFT))
+                xOffset = -10;
+            if (keysDown.Contains(KeyboardHook.VK.VK_RIGHT))
+                xOffset = 10;
+            if (xOffset != 0 || yOffset != 0)
+                MouseOperations.SetCursorPosition(new MouseOperations.MousePoint() { X = currentMousePosition.X + xOffset, Y = currentMousePosition.Y + yOffset });
+        }
+
+        private static string ToComboString(List<KeyboardHook.VK> keys)
+        {
+            string currentCombo = "";
+            for (int i = 0; i < keys.Count; i++)
+            {
+                currentCombo += keys[i];
+                if (i < keys.Count - 1)
+                    currentCombo += " + ";
+            }
+            return currentCombo;
+        }
+
+        private async void FrameRunner(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                OnUpdate();
+                if (cancellationToken.IsCancellationRequested)
+                    break;
+
+                await Task.Delay(10);
+            }
+        }
+        private void KeyboardHook_onKeyDown(KeyboardHook.VK key)
+        {
+            if (!keysDown.Contains(key))
+                keysDown.Add(key);
+        }
+        private void KeyboardHook_onKeyUp(KeyboardHook.VK key)
+        {
+            keysDown.Remove(key);
+        }
+
+        private void MacroForm_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+    }
+}
